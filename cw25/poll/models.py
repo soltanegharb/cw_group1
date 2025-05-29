a893d23a592298f5b7acb05820e39fd07f0bed52
Mohammad Hossein Soltani <soltanisoltanegharb@gmail.com>
2025-05-29 15:15:20 +0330

    Add models

--- cw25/poll/models.py
+++ cw25/poll/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(to=Poll, on_delete=models.CASCADE)
+    choice_text = models.TextField()
+    vote_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.choice_text}"
