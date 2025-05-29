a33425e7c8faab12461cb2ed80e01611b81f350d
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-05-29 15:45:36 +0330

    update models and add views

--- cw25/poll/models.py
+++ cw25/poll/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Poll(models.Model):
     question = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
+    publish_date = models.DateField(auto_now_add=True)
 
 
 class Choice(models.Model):
--- cw25/poll/views.py
+++ cw25/poll/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from poll.models import Poll, Choice
+import datetime
 
-# Create your views here.
+def show_question(request):
+    if request.method == 'POST':
+        pass
+    else:
+        today = datetime.datetime.today().date()
+        poll = Poll.objects.get(publish_date=today)
+        choices = Choice.objects.filter(question=poll)
+        context = {'poll': poll, 'choices': choices}
+        return render(
+            request,
+            'poll.html',
+            context
+        )
